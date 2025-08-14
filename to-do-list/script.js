a3db59732e80d70862287533bc544bcef60eb03b
Tyrese-Paulus <78546092+Tyrese-Paulus@users.noreply.github.com>
2025-08-14 06:08:47 +0000

    logic 70%

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,6 +1,30 @@
 const item = {
+    id: Number,
     title: String,
     status: Boolean,
 }
 
-const items = []
+
+const items = JSON.parse(localStorage.getItem('storedItems'));
+
+const displayedItems = document.getElementById('content')
+
+items.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item.title;
+    displayedItems.appendChild(li)
+})
+
+function push(){
+    const id = Date.now()
+    const title = document.getElementById("input").value;
+    const item = {
+        id: id,
+        title: title,
+        status: false,
+    }
+
+    items.push(item)
+    localStorage.setItem('storedItems', JSON.stringify(items));
+    console.log(items);
+}
