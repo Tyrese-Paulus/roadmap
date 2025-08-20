e9c65bd4b71982cafdeb344e572d5f58f08145f3
Tyrese-Paulus <78546092+Tyrese-Paulus@users.noreply.github.com>
2025-08-20 19:16:11 +0000

    3-component

---
+++ 3-component-app/src/App.js
@@ -0,0 +1,12 @@
+import Image from './components/main-image/main-image'
+import './App.css'
+
+function App() {
+  return (
+    <main className = 'background'>
+      <Image />
+    </main>
+  );
+}
+
+export default App;
---
+++ 3-component-app/src/components/main-image/main-image.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './main-image.css'
+
+function Image(){
+    return (
+       <div className="center">
+            <img className="main-img" src="https://images.pexels.com/photos/18731626/pexels-photo-18731626.jpeg"></img>
+       </div>
+    )
+}
+
+export default Image;
