8ab745958a41f14126fce97f0a03c4e59247de4c
Tyrese-Paulus <78546092+Tyrese-Paulus@users.noreply.github.com>
2025-09-03 07:17:19 +0000

    progress

--- 3-component-app/src/images.js
+++ 3-component-app/src/images.js
@@ -4,6 +4,7 @@ const images = [
     {src: "https://images.pexels.com/photos/27564614/pexels-photo-27564614.jpeg"},
     {src: "https://images.pexels.com/photos/9358816/pexels-photo-9358816.jpeg"},
     {src: "https://images.pexels.com/photos/30283880/pexels-photo-30283880.jpeg"},
+    {src: "https://images.pexels.com/photos/30283880/pexels-photo-30283880.jpeg"},
 ]
 
 export default images;
